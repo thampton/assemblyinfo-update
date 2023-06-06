95fa3128447a03af2bd1ca0f667faf2060d1880c
Travis Hampton <travis@travishampton.net>
2023-06-05 19:41:17 -0500

    Test regex from semver.org

--- SetVersion.ps1
+++ SetVersion.ps1
@@ -26,7 +26,7 @@ function SetVersion($file)
 	}
 }
 
-$isSemVer = [Regex]::Match($version, '^\d+\.\d+(\.\d+)?')
+$isSemVer = [Regex]::Match($version, '^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$')
 if ($isSemVer.success)
 {
 	if( $useBuildNumber )
